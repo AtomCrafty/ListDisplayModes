46109c9c46779fdbef00230a64769dfd25a7f11e
AtomCrafty <atomcrafty@frucost.net>
2018-09-18 19:53:49 +0200

    Initial implementation

--- list-display-modes/Program.cs
+++ list-display-modes/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using OpenTK;
 
-namespace list_display_modes {
-	class Program {
-		static void Main(string[] args) {
+namespace ListDisplayModes {
+	internal static class Program {
+		private static void Main() {for(int i = 0; ; i++) {
+				var display = DisplayDevice.GetDisplay((DisplayIndex)i);
+				if(display == null) {
+					if(i == 0) Console.WriteLine("No displays found");
+					break;
+				}
+
+				Console.WriteLine($"---- Display {i}{(display.IsPrimary ? " (Primary)" : "")} ----");
+				Console.WriteLine("Native resolution:");
+				PrintDisplayMode(display.Width, display.Height, display.RefreshRate, display.BitsPerPixel);
+				Console.WriteLine("Available resolutions:");
+				foreach(var mode in display.AvailableResolutions) {
+					PrintDisplayMode(mode.Width, mode.Height, mode.RefreshRate, mode.BitsPerPixel);
+				}
+				Console.WriteLine();
+			}
+			Console.ReadLine();
+		}
+
+		private static void PrintDisplayMode(int width, int height, float refreshRate, int bitsPerPixel) {
+			Console.WriteLine($"    {width}x{height}".PadRight(14) + $"@{refreshRate:f2}Hz".PadRight(10) + $"{bitsPerPixel}bpp");
 		}
 	}
 }
