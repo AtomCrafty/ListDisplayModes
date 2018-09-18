9b08cc1186f1b7e6a72cb3c70601467d93904be2
AtomCrafty <atomcrafty@frucost.net>
2018-09-18 20:04:05 +0200

    Selectable display order

--- list-display-modes/Program.cs
+++ list-display-modes/Program.cs
@@ -1,10 +1,33 @@
 ﻿using System;
+using System.ComponentModel.Design;
 using System.Linq;
+using System.Net.Sockets;
 using OpenTK;
 
 namespace ListDisplayModes {
 	internal static class Program {
-		private static void Main() {for(int i = 0; ; i++) {
+
+		private static void Main() {
+
+			var orderFuncs = new Func<DisplayResolution, float>[] {
+				mode => mode.Width * mode.Height,
+				mode => mode.Width,
+				mode => mode.Height,
+				mode => mode.RefreshRate,
+				mode => mode.BitsPerPixel
+			};
+
+			Console.WriteLine("Specify order priority");
+			Console.WriteLine(" [0] Screen area (default)");
+			Console.WriteLine(" [1] Width");
+			Console.WriteLine(" [2] Height");
+			Console.WriteLine(" [3] Refresh rate");
+			Console.WriteLine(" [4] Color depth");
+
+			int selection = int.TryParse(Console.ReadLine(), out selection) && selection <= 4 ? selection : 0;
+			var order = orderFuncs[selection];
+
+			for(int i = 0; ; i++) {
 				var display = DisplayDevice.GetDisplay((DisplayIndex)i);
 				if(display == null) {
 					if(i == 0) Console.WriteLine("No displays found");
@@ -15,7 +38,7 @@ namespace ListDisplayModes {
 				Console.WriteLine("Native resolution:");
 				PrintDisplayMode(display.Width, display.Height, display.RefreshRate, display.BitsPerPixel);
 				Console.WriteLine("Available resolutions:");
-				foreach(var mode in display.AvailableResolutions) {
+				foreach(var mode in display.AvailableResolutions.OrderByDescending(order)) {
 					PrintDisplayMode(mode.Width, mode.Height, mode.RefreshRate, mode.BitsPerPixel);
 				}
 				Console.WriteLine();
